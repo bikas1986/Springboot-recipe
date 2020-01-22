03d4cd1c86a854de52e64e602f8531b4c892e4d9
bikas <bikas@192.168.1.45>
2020-01-22 22:48:29 +0530

    My first commit and initial project setup.

---
+++ src/main/java/com/bikas/springbootrecipe/SpringBootRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.bikas.springbootrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikas/springbootrecipe/SpringBootRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikas.springbootrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
